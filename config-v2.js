2ae5447064e151296b89128469cb39ad1f463e20
kingbrthr <rehmankingg143@gmail.com>
2026-03-26 02:37:23 +0500

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const DATABASE = "postgres://koyeb-adm:npg_z1fDlsGoXQP7@ep-orange-violet-a1p2ny7k.ap-southeast-1.pg.koyeb.app/koyebdb" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
-const BOT_NUMBER = "32467410178" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
-const SESSION_ID = "PRABATH-MD~IFVy0BAxxJa44ne" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
+const DATABASE = "" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
+const BOT_NUMBER = "" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
+const SESSION_ID = "" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
 
 // ඉහත ඒව "" මැදට එකතු කරන්න වෙනත් කිසිම දෙයක් වෙනස් නොකරන්න ❌. (Add "" in the middle of the above and ❌ don't change anything else.)
